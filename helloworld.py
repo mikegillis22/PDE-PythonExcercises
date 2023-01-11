068791183f213df7f05339d0002b3d2a2e84ad87
Mike Gillis <mikegillis22@gmail.com>
2023-01-11 00:57:40 +0000

    Pending changes exported from your codespace

---
+++ helloworld.py
@@ -0,0 +1,3 @@
+# this is my first python program
+
+print("Hello World!")
---
+++ second.py
@@ -0,0 +1,6 @@
+# my second program
+
+colors = ["red", "blue", "green", "magenta"]
+
+for color in colors:
+    print(color)
